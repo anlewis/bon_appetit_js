8b4b81dd48c63be2e02f01ccc1167630510ce40b
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-04 10:28:09 -0600

    Add restock function to pantry

--- test/pantry.test.js
+++ test/pantry.test.js
@@ -18,5 +18,24 @@ describe("Pantry", () => {
         expect(pantry.stockCheck("Cheese")).to.eql(0)
       })
     })
+
+    describe("restock", () => {
+      it("Adds ingredient of a given amount to the pantry stock", () => {
+        const pantry = new Pantry()
+        pantry.restock("Cheese", 10)
+
+        expect(pantry.stockCheck("Cheese")).to.eql(10)
+      })
+    })
+
+    describe("restock", () => {
+      it("Adds ingredient of a given amount to the pantry stock, summing amount if ingredient is in stock", () => {
+        const pantry = new Pantry()
+        pantry.restock("Cheese", 10)
+        pantry.restock("Cheese", 20)
+
+        expect(pantry.stockCheck("Cheese")).to.eql(30)
+      })
+    })
   })
 })
